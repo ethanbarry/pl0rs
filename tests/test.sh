a4e6db282a825b695d68a7b9abcf865d0ff50d4c
Ethan Barry <ethanbarry@howdytx.net>
2024-05-07 22:41:18 -0500

    Fixed tests.

--- tests/test.sh
+++ tests/test.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
-echo PL/0 compiler test suite
-echo ========================
+echo ╔══════════════════════════╗
+echo ║ PL/0 compiler test suite ║
+echo ╚══════════════════════════╝
+echo ░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+echo
 
 for i in *.pl0 ; do
   /usr/bin/printf "%.4s... " $i
-  ../target/release/pl-zero-rs < $i > /dev/null 2>&1
+  ../target/release/pl-zero-rs -f $i > /dev/null 2>stderror.txt
   if [ $? -eq 0 ] ; then
     echo ok
   else
     echo fail
   fi
 done
+
+if [ -s stderror.txt ] ; then
+   echo "stderror.txt is not empty!"
+   cat stderror.txt
+else
+   rm stderror.txt
+fi
